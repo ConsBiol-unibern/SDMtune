8dbfefb031ecb53676309cfce89383b9dbc8c5d9
Sergio Vignali <vignalisergio30@gmail.com>
2018-07-15 20:13:51 +0200

    Add predict function

--- R/predict.R
+++ R/predict.R
@@ -1,4 +1,4 @@
-.predict_from_lambdas <- function(model, data, output_format, clamp = TRUE) {
+.predict_from_lambdas <- function(model, data, maxent_output, clamp = TRUE) {
 
     if (clamp) {
       for (variable in model@min_max$variable) {
@@ -16,9 +16,9 @@
 
     S <- (dm %*% model@coeff$lambda) - model@lpn
     raw <- exp(S) / model@dn
-    if (output_format == "raw") {
+    if (maxent_output == "raw") {
       return(raw)
-    } else if (output_format == "logistic") {
+    } else if (maxent_output == "logistic") {
        return(raw * exp(model@entropy) / (1 + raw * exp(model@entropy)))
     } else {
       return(raw * exp(model@entropy) / (1 + raw * exp(model@entropy)))
@@ -28,39 +28,49 @@
 #' Predict Maxent
 #'
 #' @param model A Maxent model object.
-#' @param data A data frame or a raster Stack.
-#' @param clamp Flag for doing clumping during prediction, default is TRUE.
+#' @param data A data frame, a \link[stack:raster] or \link{brick}.
+#' @param clamp Flag for clumping during prediction, default is TRUE.
+#' @param maxent_output The output type, possible values are "raw", "logistic" or "cloglog".
+#' @param filename Output file name for the prediction map. If not provided the output is
+#' saved in a file.
+#' @param format The output format, see \link{writeRaster} for all the options, default is "GTiff".
+#' @param extent \link{Extent} object, if provided it restricts the prediction to the given
+#' extent, default is NULL.
 #' @param parallel Flag to use parallel computation during prediction, default is FALSE.
-#' @param output_format The format of the output, possible values are "raw", "logistic" or "cloglog".
 #'
-#' @details You need package snow to use parallel computation.
+#' @details You need package \link{snow} to use parallel computation.
 #'
 #' @return A vector of prediction or a Raster object if data is a raster stack.
 #'
 #' @examples\dontrun{
-#' predic(model, predictors, output_format = "cloglog", parallel = TRUE)}
+#' predic(model, predictors, maxent_output = "cloglog", parallel = TRUE)}
 #'
 #' @author Sergio Vignali
-predict.Maxent <- function(model, data, clamp = TRUE, parallel = FALSE,
-                           output_format = c("logistic", "cloglog", "raw")) {
+predict.Maxent <- function(model, data, clamp = TRUE,
+                           maxent_output = c("logistic", "cloglog", "raw"),
+                           filename = "", format = "GTiff", extent = NULL,
+                           parallel = FALSE) {
 
   start_time <- proc.time()
-  format <- match.arg(output_format)
+  maxent_output <- match.arg(maxent_output)
 
   if (inherits(data, "Raster")) {
     if (parallel) {
       beginCluster()
-      p <- clusterR(data, predict, args = list(model = model,  clamp = clamp,
-                                              output_format = format,
-                                              fun = .predict_from_lambdas,
-                                              progress = 'text'))
+      p <- clusterR(data, predict, args = list(model = model,
+                                               clamp = clamp,
+                                               maxent_output = maxent_output,
+                                               fun = .predict_from_lambdas),
+                    progress = 'text', filename = filename, format = format,
+                    ext = extent)
       endCluster()
     } else {
-      p <- predict(data, model = model, output_format = format, clamp = clamp,
-                   fun = .predict_from_lambdas, progress = 'text')
+      p <- predict(data, model = model, maxent_output = maxent_output,
+                   clamp = clamp, fun = .predict_from_lambdas, progress = 'text',
+                   filename = filename, format = format, ext = extent)
     }
   } else if (inherits(data, "data.frame")) {
-    p <- .predict_from_lambdas(model, data, output_format = format,
+    p <- .predict_from_lambdas(model, data, maxent_output = maxent_output,
                                clamp = clamp)
   }
 
