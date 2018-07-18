0a6344863246bea2b40f8e136935f63ee80b3bd0
Sergio Vignali <vignalisergio30@gmail.com>
2018-07-18 21:13:46 +0200

    Add onAttach function

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(" ____________________________________________________________________________")
+  packageStartupMessage("|   ____   ____   __  __  ____         _              _    _                 |")
+  packageStartupMessage("|  / ___| |  _ \\ |  \\/  |/ ___|   ___ | |  ___   ___ | |_ (_)  ___   _ __    |")
+  packageStartupMessage("|  \\___ \\ | | | || |\\/| |\\___ \\  / _ \\| | / _ \\ / __|| __|| | / _ \\ | '_ \\   |")
+  packageStartupMessage("|   ___) || |_| || |  | | ___) ||  __/| ||  __/| (__ | |_ | || (_) || | | |  |")
+  packageStartupMessage("|  |____/ |____/ |_|  |_||____/  \\___||_| \\___| \\___| \\__||_| \\___/ |_| |_|  |")
+  packageStartupMessage("|____________________________________________________________________________|")
+  packageStartupMessage("")
+}
