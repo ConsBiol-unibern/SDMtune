cddb0b8a5c14fd415666901a055e4248c37b7e0a
Sergio Vignali <vignalisergio30@gmail.com>
2018-07-18 17:36:34 +0200

    Add response method

--- R/response.R
+++ R/response.R
@@ -1,112 +1,119 @@
+setGeneric("response", function(object, ...)
+  standardGeneric("response")
+)
 #' Response Curve
 #'
 #' Plot the Responce Courve of the given environmental variable
 #'
-#' @param model A MaxentModel object.
-#' @param variable A variable name, used to plot only one variable.
-#' @param marginal Flag to plot the marginal response courves, default is FALSE.
-#' @param rug Flag to add the rug plot for presence locations, available only for continuous variables,
+#' @param object Maxent object.
+#' @param variable character. Name of the variable to be plotted.
+#' @param maxent_output character. The Maxent output type, possible values are "raw", "logistic" or "cloglog".
+#' @param marginal logical, if TRUE it plots the marginal response courve, default is FALSE.
+#' @param clamp logical for clumping during prediction, default is TRUE.
+#' @param rug logical, if TRUE it adds the rug plot for presence and background locations locations,
+#' available only for continuous variables,
 #' default is FALSE.
 #' @param color The color of the curve, default is "red".
 #'
+#' @include Maxent_class.R
+#' @exportMethod response
+#'
 #' @return The plot objects
-#' @export
+#' @importFrom raster modal
 #'
 #' @examples
-#' \dontrun{responce(model, variable = 'bio12', marginal = TRUE, rug = TRUE)}
+#' \dontrun{
+#' response(model, variable = "bio12", maxent_output = "cloglog", marginal = TRUE, rug = TRUE)}
 #'
 #' @author Sergio Vignali
-response <- function(model, variable,
-                     maxent_output = c("cloglog", "logistic", "raw"),
-                     clamp = TRUE, marginal = FALSE, rug = FALSE,
-                     color = "red") {
-  if (class(model) != "Maxent")
-    stop("Model must be a Maxent object!")
+setMethod("response",
+          signature = "Maxent",
+          definition = function(object, variable,
+                                maxent_output = c("cloglog", "logistic", "raw"),
+                                marginal = FALSE, clamp = TRUE, rug = FALSE,
+                                color = "red") {
 
-  if (!variable %in% names(model@presence@data))
-    stop(paste(variable, "is not used to train the model!"))
+            if (!variable %in% names(object@presence@data))
+              stop(paste(variable, "is not used to train the model!"))
 
-  cont_vars <- names(Filter(is.numeric, model@presence@data))
-  cat_vars <- names(Filter(is.factor, model@presence@data))
-  maxent_output <- match.arg(maxent_output)
+            cont_vars <- names(Filter(is.numeric, object@presence@data))
+            cat_vars <- names(Filter(is.factor, object@presence@data))
+            maxent_output <- match.arg(maxent_output)
 
-  train <- model@presence
-  bg <- model@background
-  train_rug <- data.frame(x = model@presence@data[, variable])
-  bg_rug <- data.frame(x = model@background@data[, variable])
+            if (variable %in% cat_vars) {
+              categ <- unique(as.numeric(levels(bg@data[, variable]))[bg@data[, variable]])
+              n_rows <- length(categ)
+            } else {
+              n_rows <- 100
+            }
 
-  if (marginal) {
-    train@data <- model@presence@data[variable]
-    bg@data <- model@background@data[variable]
-    model <- trainMaxent(train, bg, rm = model@rm, fc = model@fc,
-                         maxent_output = maxent_output,
-                         iterations = model@iterations)
+            train <- object@presence
+            bg <- object@background
+            train_rug <- data.frame(x = object@presence@data[, variable])
+            bg_rug <- data.frame(x = object@background@data[, variable])
+            data <- data.frame(matrix(NA, nrow = 1, ncol = ncol(train@data)))
+            colnames(data) <- colnames(train@data)
+            data[cont_vars] <- apply(train@data[cont_vars], 2, mean)
+            data[cat_vars] <- apply(train@data[cat_vars], 2, raster::modal)
+            data <- do.call("rbind", replicate(n_rows, data, simplify = FALSE))
 
-    data <- mergeSWD(train, bg)
-    if (clamp & !variable %in% cat_vars ) {
-      data <- data.frame(x = data@data[, 1])
-      data <- subset(data, x >= model@min_max[1, ]$min &
-                     x <= model@min_max[1, ]$max)
-      colnames(data) <- variable
-      train_rug <- subset(train_rug, x >= model@min_max[1, ]$min &
-                          x <= model@min_max[1, ]$max)
-      bg_rug <- subset(bg_rug, x >= model@min_max[1, ]$min &
-                       x <= model@min_max[1, ]$max)
-    } else {
-      data <- data@data
-      if (variable %in% cat_vars) {
-        categ <- unique(as.numeric(levels(data[, 1]))[data[, 1]])
-        data <- data.frame(x <- categ)
-        colnames(data) <- variable
-      }
-    }
-  } else {
-    if (clamp) {
-      for (var in model@min_max$variable) {
-        data[var] <- raster::clamp(data[, var],
-                                   model@min_max$min[model@min_max$variable == var],
-                                   model@min_max$max[model@min_max$variable == var])
-      }
-      train_rug <- subset(train_rug, x >= model@min_max[1,]$min &
-                            x <= model@min_max[1,]$max)
-      bg_rug <- subset(bg_rug, x >= model@min_max[1,]$min &
-                         x <= model@min_max[1,]$max)
-    }
+            if (clamp & variable %in% cont_vars) {
+              var_min <- min(bg@data[variable])
+              var_max <- max(bg@data[variable])
+              train_rug <- subset(train_rug, x >= var_min & x <= var_max)
+            } else if (variable %in% cont_vars) {
+              var_min <- min(rbind(train@data[variable], bg@data[variable]))
+              var_max <- max(rbind(train@data[variable], bg@data[variable]))
+            }
 
-  }
+            if (variable %in% cont_vars) {
+              data[variable] <- seq(var_min, var_max, length.out = n_rows)
+            } else {
+              data[variable] <- categ
+            }
 
-  pred <- suppressMessages(predict(model, data, maxent_output = maxent_output,
-                                   clamp = FALSE))
-  plot_data <- data.frame(x = data[, variable], y = pred)
+            if (marginal) {
+              train@data <- object@presence@data[variable]
+              bg@data <- object@background@data[variable]
+              object <- trainMaxent(train, bg, rm = object@rm, fc = object@fc,
+                                   maxent_output = maxent_output,
+                                   iterations = object@iterations)
+            }
 
-  if (variable %in% cont_vars) {
-    my_plot <- ggplot() +
-      geom_line(data = plot_data, aes(x = x, y = y), colour = color)
+            pred <- suppressMessages(predict(object, data,
+                                             maxent_output = maxent_output,
+                                             clamp = clamp))
+            plot_data <- data.frame(x = data[, variable], y = pred)
 
-  } else {
-    my_plot <- ggplot(data = plot_data, aes(x = x, y = y)) +
-      geom_bar(stat = "identity", fill = color) +
-      scale_x_continuous(breaks = seq(min(plot_data$x), max(plot_data$x), 1))
-  }
+            if (variable %in% cont_vars) {
+              my_plot <- ggplot(plot_data, aes(x = x, y = y)) +
+                geom_line(colour = color, size = 1)
 
-  my_plot <- my_plot +
-    ggtitle(model@presence@species) +
-    xlab(variable) +
-    ylab(paste(maxent_output, "output")) +
-    theme(plot.title = element_text(hjust = 0.5, face = "italic"),
-          legend.position = "none",
-          axis.ticks.x = element_blank(),
-          axis.ticks.y = element_blank())
+            } else {
+              my_plot <- ggplot(plot_data, aes(x = x, y = y)) +
+                geom_bar(stat = "identity", fill = color) +
+                scale_x_continuous(breaks = seq(min(plot_data$x),
+                                                max(plot_data$x), 1))
+            }
 
-  if (rug == TRUE & variable %in% cont_vars) {
-    my_plot <- my_plot +
-      geom_rug(data = train_rug, inherit.aes = FALSE, aes(x),
-               sides = "t", color = "#4C4C4C") +
-      geom_rug(data = bg_rug, inherit.aes = FALSE, aes(x),
-               sides = "b", color = "#4C4C4C")
-  } else if (rug == TRUE) {
-    message("Warning: rug is available only for continous variables!")
-  }
+            my_plot <- my_plot +
+              ggtitle(object@presence@species) +
+              xlab(variable) +
+              ylab(paste(maxent_output, "output")) +
+              theme(plot.title = element_text(hjust = 0.5, face = "italic"),
+                    legend.position = "none",
+                    axis.ticks.x = element_blank(),
+                    axis.ticks.y = element_blank())
 
-  return(my_plot)
-}
+            if (rug == TRUE & variable %in% cont_vars) {
+              my_plot <- my_plot +
+                geom_rug(data = train_rug, inherit.aes = FALSE, aes(x),
+                         sides = "t", color = "#4C4C4C") +
+                geom_rug(data = bg_rug, inherit.aes = FALSE, aes(x),
+                         sides = "b", color = "#4C4C4C")
+            } else if (rug == TRUE) {
+              message("Warning: rug is available only for continous variables!")
+            }
+
+            return(my_plot)
+          })
