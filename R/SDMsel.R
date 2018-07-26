27af835ad4670d0c8ab4a0331ccece0db603fd40
Sergio Vignali <vignalisergio30@gmail.com>
2018-07-26 18:39:48 +0200

    Add pkg description and upgrade ggplo2

---
+++ R/SDMsel.R
@@ -0,0 +1,11 @@
+#' SDMsel: A package for tuning Species Distribution Models.
+#'
+#' Performs data driven variable selection and model tuning for SDMs
+#' (Species Distribution Models). It provides also some utilities to display
+#' results (at the moment it implements only MaxEnt models).
+#'
+#' The foo functions ...
+#'
+#' @docType package
+#' @name SDMsel
+NULL
