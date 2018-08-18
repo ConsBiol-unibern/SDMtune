53897e31fb525e76350143203f0856f9967e30aa
Sergio Vignali <vignalisergio30@gmail.com>
2018-08-18 16:55:29 +0200

    Adapt plotVarImp

--- R/plotVarImp.R
+++ R/plotVarImp.R
@@ -1,10 +1,9 @@
 #' Plot Varible Importance
 #'
-#' Plot the variable importance as a bar plot.
+#' Plot the permutation variable importance as a bar plot.
 #'
-#' @param model Maxent object.
+#' @param df data.frame. The result of varImp function.
 #' @param color character. The color of the bar plot, default is grey.
-#' @param type character, contribution or permutation (or an abbreviation of them) are possible values.
 #'
 #' @return The ggplot object.
 #' @export
@@ -13,35 +12,21 @@
 #'
 #' @examples
 #' \dontrun{
-#' plotContribution(model, type = "permut", color = '#a1d99b')}
+#' plotVarImp(my_table, color = '#a1d99b')}
 #'
 #' @author Sergio Vignali
-plotVarImp <- function(model, type = c("contribution", "permutation"),
-                       color = "grey") {
+plotVarImp <- function(df, color = "grey") {
 
-  if (class(model) != "Maxent") stop("Model must be a Maxent object!")
+  df <- df[order(df[, 2]), ]
+  df[, 2] <- df[, 2] / 100
+  df[, 1] <- factor(df[, 1], levels = df[, 1])
+  my_plot <- ggplot(df, aes_(x = ~Variable, y = ~Permutation_importance)) +
+    ylab("Permutation Importance")
 
-  type <- match.arg(type)
-  df <- varImp(model)
-
-  if (type == "contribution") {
-    df <- df[nrow(df):1, ]
-    df$Percent_contribution <- df$Percent_contribution / 100
-    df$Variable <- factor(df$Variable, levels = df$Variable)
-    my_plot <- ggplot(df, aes_(x = ~Variable, y = ~Percent_contribution)) +
-      ylab("Percent Contribution")
-  } else {
-    df <- df[order(df$Permutation_importance), ]
-    df$Permutation_importance <- df$Permutation_importance / 100
-    df$Variable <- factor(df$Variable, levels = df$Variable)
-    my_plot <- ggplot(df, aes_(x = ~Variable, y = ~Permutation_importance)) +
-      ylab("Permutation Importance")
-  }
   my_plot <- my_plot +
     scale_y_continuous(labels = scales::percent) +
     geom_bar(position = "dodge", stat = "identity", fill = color) +
     coord_flip() +
-    ggtitle(model@presence@species) +
     xlab("") +
     theme(plot.title = element_text(hjust = 0.5, face = "italic"),
           axis.ticks.x = element_blank(),
