577d1a2314401853328a2ba5e8505f837d42afb5
Sergio Vignali <vignalisergio30@gmail.com>
2018-08-22 08:53:36 +0200

    Some more work on render template function

--- R/render_template.R
+++ R/render_template.R
@@ -7,19 +7,27 @@
 #' @export
 #'
 #' @examples
-render_template <- function(model, folder) {
+render_template <- function(model, test, folder) {
   template = system.file("templates", "modelOutput.Rmd", package = "SDMsel")
 
-  dir.create(folder)
+  dir.create(paste0(folder, "/plots"), recursive = TRUE)
   folder <- paste0(getwd(), "/", folder)
+  plot_folder <- paste0(folder, "/plots")
   species <- gsub(" ", "_", tolower(model@presence@species))
+  title <- paste(class(model@model), "model for", model@presence@species)
+  args <- c(paste0("--metadata=title:\"", title, "\""))
 
-  args <- c(paste0("--metadata=title:\"", model@presence@species, "\""))
+  roc <- plotROC(model, test = test)
+  ggsave(filename = "ROC",
+         plot = roc,
+         device = "png",
+         path = plot_folder,
+         width = 12, height = 12, units = "cm")
 
   rmarkdown::render(template,
                     output_file = paste0(species, ".html"),
                     output_dir = folder,
-                    params = list(model = model),
+                    params = list(model = model, test = test),
                     output_options = list(pandoc_args = args),
                     quiet = TRUE
                     )
