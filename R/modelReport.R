3699d08313f056d806a15bb9561aa5ddd2b53eae
sgvignali <vignalisergio30@gmail.com>
2018-08-22 16:04:58 +0200

    Fix plot curves in report

--- R/modelReport.R
+++ R/modelReport.R
@@ -7,31 +7,32 @@
 #' @export
 #'
 #' @examples
-render_template <- function(model, type, test, folder) {
-  template = system.file("templates", "modelOutput.Rmd", package = "SDMsel")
+modelReport <- function(model, type, test, folder) {
+  template = system.file("templates", "modelReport.Rmd", package = "SDMsel")
 
   dir.create(paste0(folder, "/plots"), recursive = TRUE)
   folder <- paste0(getwd(), "/", folder)
-  # plot_folder <- paste0(folder, "/plots")
+  plot_folder <- paste0(folder, "/plots")
   species <- gsub(" ", "_", tolower(model@presence@species))
   title <- paste(class(model@model), "model for", model@presence@species)
   args <- c(paste0("--metadata=title:\"", title, "\""))
 
-  # roc <- plotROC(model, test = test)
-  # ggsave(filename = "ROC",
-  #        plot = roc,
-  #        device = "png",
-  #        path = plot_folder,
-  #        width = 12, height = 12, units = "cm")
-
   rmarkdown::render(template,
                     output_file = paste0(species, ".html"),
                     output_dir = folder,
-                    params = list(model = model, type = type, test = test),
+                    params = list(model = model, type = type, test = test,
+                                  plot_folder = plot_folder),
                     output_options = list(pandoc_args = args),
                     quiet = TRUE
                     )
 
+  # # ggsave in a Rmd file doesn't save the device...
+  # for (file in list.files(path = plot_folder))
+  #   file.rename(file, paste0(file, ".png"))
+
   html <- paste0(folder, "/", species, ".html")
   browseURL(html)
+
+  return(invisible(html))
 }
+
