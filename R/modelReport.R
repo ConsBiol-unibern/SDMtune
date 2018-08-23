8f8d9c43d5d594537bdc7ff4184147d23527aa0a
Sergio Vignali <vignalisergio30@gmail.com>
2018-08-23 12:24:27 +0200

    Add jk to template and optional arguments to modelReport

--- R/modelReport.R
+++ R/modelReport.R
@@ -1,13 +1,28 @@
-#' Title
+#' Model Report
 #'
-#' @param model
-#' @param folder
+#' Make a model report that shows main results
 #'
-#' @return
+#' @param model SDMmodel object
+#' @param type character. Output type, see \link{predict,Maxent-method} for
+#' Maxent models or \link{predict.maxnet} for Maxnet models.
+#' @param folder character. The name of the folder in which to save the output.
+#' The folder is created in the working directory.
+#' @param test SWD object with the test locations, default is NULL.
+#' @param response_curves logical, if TRUE it plots the response curves in the
+#' html output, default is FALSE.
+#' @param jk logical, if TRUE it runs the jackknife test, default FALSE.
+#'
+#' @details The function produces a report similar to the one created by Maxent
+#' software.
+#'
 #' @export
 #'
-#' @examples
-modelReport <- function(model, type, test, folder) {
+#' @examples \dontrun{
+#' modelReport(model, "cloglog", folder = "my_folder", response_curves = T)}
+#'
+#' @author Sergio Vignali
+modelReport <- function(model, type, folder, test = NULL,
+                        response_curves = FALSE, jk = FALSE) {
   template = system.file("templates", "modelReport.Rmd", package = "SDMsel")
 
   dir.create(paste0(folder, "/plots"), recursive = TRUE)
@@ -21,18 +36,13 @@ modelReport <- function(model, type, test, folder) {
                     output_file = paste0(species, ".html"),
                     output_dir = folder,
                     params = list(model = model, type = type, test = test,
-                                  plot_folder = plot_folder),
+                                  folder = plot_folder,
+                                  response_curves = response_curves, jk = jk),
                     output_options = list(pandoc_args = args),
                     quiet = TRUE
                     )
 
-  # # ggsave in a Rmd file doesn't save the device...
-  # for (file in list.files(path = plot_folder))
-  #   file.rename(file, paste0(file, ".png"))
-
   html <- paste0(folder, "/", species, ".html")
   browseURL(html)
-
-  return(invisible(html))
 }
 
