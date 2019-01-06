c5fd56fcfb3ba0095d795ec0e7811bceb90d2aad
Sergio Vignali <vignalisergio30@gmail.com>
2019-01-06 13:20:19 +0100

    Add plot method for SDMtune object

--- R/SDMtune_class.R
+++ R/SDMtune_class.R
@@ -20,3 +20,74 @@ setMethod("show",
     cat("Feature Classes      : ", paste(unique(object@results$fc), collapse = ", "), "\n")
   }
 )
+
+if (!isGeneric("plot"))
+  setGeneric("plot", function(x, y, ...) standardGeneric("plot"))
+
+#' Plot
+#'
+#' Plot the result of a tune function or of the optimiseModel function.
+#'
+#' @param SDMtune SDMtune object.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{plot(my_tuneBg)}
+#'
+#' @author Sergio Vignali
+setMethod("plot",
+  signature(x = "SDMtune", y = "missing"),
+  definition = function(x, y, ...) {
+    res <- x@results
+    n <- nrow(res)
+
+    # Get metric
+    if (colnames(res)[4] == "train_AUC") {
+      metric <- "AUC"
+    } else if (colnames(res)[4] == "train_TSS") {
+      metric <- "TSS"
+    } else {
+      metric <- "AICc"
+    }
+
+    if (length(unique(res$fc)) != 1 & length(unique(res$bg)) != 1) {
+      #  Result of modelOptimise function
+      x_label <- "Model"
+      x <- 1:n
+    } else if (length(unique(res$fc)) == 1 & length(unique(res$bg)) != 1) {
+      #  Result of tuneBg function
+      x_label <- "Backgrounds"
+      x <- res[, 1]
+    } else if (length(unique(res$fc)) != 1 & length(unique(res$bg)) == 1) {
+      #  Result of tuneFC function
+      x_label <- "Feature Combination"
+      x <- res[, 3]
+    } else {
+      #  Result of tuneReg function
+      x_label <- "Regularization"
+      x <- res[, 2]
+    }
+
+    if (metric != "AICc") {
+      data <- data.frame(x = rep(x, 2), y = c(res[, 4], res[, 5]),
+                         type = c(rep("Training", n), rep("Validation", n)))
+    } else {
+      data <- data.frame(x = x, y = res[, 4], type = rep("Training", n))
+    }
+
+    #  Create scatterplot
+    p <- ggplot(data, aes_string(x = "x", y = "y", colour = "type",
+                                 group = "type")) +
+      geom_point() +
+      labs(x = x_label, y = metric) +
+      scale_colour_discrete(name = "") +
+      theme_minimal() +
+      theme(legend.position = "bottom")
+
+    # Add line if is the rusult of a tune function
+    if (x_label != "Model")
+      p <- p + geom_line()
+
+    return(p)
+  })
