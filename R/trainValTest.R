08ef4cfe1b19f25392af80b47115547fb73414cc
sgvignali <vignalisergio30@gmail.com>
2019-02-11 12:21:47 +0100

    Pass SWD object to prepareSWD instead of a data frame

--- R/trainValTest.R
+++ R/trainValTest.R
@@ -3,7 +3,7 @@
 #' Split a dataset randomly in train and test datasets or train, validation and
 #' test datasets.
 #'
-#' @param df data.frame containing the data that have to be splitted in train,
+#' @param x SWD object containing the data that have to be splitted in train,
 #' validation and test datasets.
 #' @param test numeric. The percentage of data withhold for testing.
 #' @param val numeric. The percentage of data withhold for validation, default
@@ -20,25 +20,51 @@
 #' sets <- trainValTest(presence, val = 0.2, test = 0.2, set_seed = 25)}
 #'
 #' @author Sergio Vignali
-trainValTest <- function(df, test, val = 0, seed = NULL) {
+trainValTest <- function(x, test, val = 0, seed = NULL) {
+
+  if (class(x) != "SWD")
+    stop("x must be an SWD object!")
 
   if (!is.null(seed)) set.seed(seed)
 
-  permutation <- sample(nrow(df))
+  n <- nrow(x@data)
+  permutation <- sample(n)
 
   if (val > 0) {
-    start_val <- round(nrow(df) * (1 - val - test), 0) + 1
-    start_test <- round(nrow(df) * val, 0) + start_val
-    train <- df[permutation[1:(start_val - 1)], ]
-    val <- df[permutation[start_val:(start_test - 1)], ]
-    test <- df[permutation[start_test:nrow(df)], ]
+    start_val <- round(n * (1 - val - test), 0) + 1
+    start_test <- round(n * val, 0) + start_val
+    train <- val <- test <- SWD()
+    train@data <- x@data[permutation[1:(start_val - 1)], ]
+    train@coords <- x@coords[permutation[1:(start_val - 1)], ]
+    # Reset row names
+    rownames(train@data) <- NULL
+    rownames(train@coords) <- NULL
+    val@data <- x@data[permutation[start_val:(start_test - 1)], ]
+    val@coords <- x@coords[permutation[start_val:(start_test - 1)], ]
+    # Reset row names
+    rownames(val@data) <- NULL
+    rownames(val@coords) <- NULL
+    test@data <- x@data[permutation[start_test:n], ]
+    test@coords <- x@coords[permutation[start_test:n], ]
+    # Reset row names
+    rownames(test@data) <- NULL
+    rownames(test@coords) <- NULL
 
     return(list(train, val, test))
 
   } else {
-    start_test <- round(nrow(df) * (1 - test), 0) + 1
-    train <- df[permutation[1:(start_test - 1)], ]
-    test <- df[permutation[start_test:nrow(df)], ]
+    start_test <- round(n * (1 - test), 0) + 1
+    train <- test <- SWD()
+    train@data <- x@data[permutation[1:(start_test - 1)], ]
+    train@coords <- x@coords[permutation[1:(start_test - 1)], ]
+    # Reset row names
+    rownames(train@data) <- NULL
+    rownames(train@coords) <- NULL
+    test@data <- x@data[permutation[start_test:n], ]
+    test@coords <- x@coords[permutation[start_test:n], ]
+    # Reset row names
+    rownames(test@data) <- NULL
+    rownames(test@coords) <- NULL
 
     return(list(train, test))
   }
