25a6366b384a3999730eeedb4d8ecfcb475c87ee
Sergio Vignali <vignalisergio30@gmail.com>
2019-02-25 13:18:01 +0100

    Add more tests

--- R/gridSearch.R
+++ R/gridSearch.R
@@ -21,40 +21,62 @@ gridSearch <- function(model, hypers, metric, test = NULL, bg4test = NULL,
 
   .checkGridSearchArgs(model, hypers, metric, test, bg4test, env)
 
-  # Create data frame with all possible combinations of hyperparameters
-  tunable_args <- .get_train_args(model)[get_tunable_args(model)]
-  tunable_args[names(hypers)] <- hypers
-  if (is.null(hypers$a))
-    tunable_args$a <- nrow(model@a@data)
-  grid <- expand.grid(tunable_args, stringsAsFactors = FALSE)
+  if (class(model) == "SDMmodelCV")
+    test <- TRUE
+
+  grid <- .get_hypers_grid(model, hypers)
 
   pb <- progress::progress_bar$new(
     format = "Grid search [:bar] :percent in :elapsedfull",
     total = (nrow(grid) + 1), clear = FALSE, width = 60, show_after = 0)
   pb$tick(0)
 
+  x_title <- .get_x_grid_title(hypers)
+  x_labels <- .get_x_grid_labels(hypers, grid)
+
   # Loop through all the settings in grid
   for (i in 1:nrow(grid)) {
     message(grid[i, ])
+    pb$tick(1)
   }
+  pb$tick(1)
 }
 
 .checkGridSearchArgs <- function(model, hypers, metric, test = NULL,
                                  bg4test = NULL, env = NULL) {
+  # Throws exception if metric is aicc and env is not provided
   if (metric == "aicc" & is.null(env) & class(model) == "SDMmodel")
     stop("You must provide the 'env' argument if you want to use the AICc metric!")
-
+  # Throws exception if model is SDMmodel metric is not aicc and test is not provided
   if (class(model) == "SDMmodel" & is.null(test) & metric != "aicc") {
     stop("You need to provide a test dataset!")
   }
-  if (class(model) == "SDMmodelCV") {
-    test <- TRUE
-    if (metric == "aicc")
-      stop("Metric 'aicc' not allowed with SDMmodelCV objects!")
-  }
+  # Throws exception if metric is aicc and model is SDMmodelCV
+  if (class(model) == "SDMmodelCV" & metric == "aicc")
+    stop("Metric 'aicc' not allowed with SDMmodelCV objects!")
+  # Throws exception if hypers includes 'a' and bg4test is not provided
   if (!is.null(hypers$a) & is.null(bg4test))
     stop("bg4test must be provided to tune background locations!")
-  if (!is.null(hypers$a) & max(hypers$a) > nrow(bg4test@data))
-    stop(paste0("Maximum number of 'a' hyperparameter cannot be more than ",
-                nrow(bg4test@data), "!"))
+  # Throws exception if max hypers 'a' > than nrow bg4test
+  if (!is.null(hypers$a)) {
+    if (max(hypers$a) > nrow(bg4test@data))
+      stop(paste0("Maximum number of 'a' hyperparameter cannot be more than ",
+                  nrow(bg4test@data), "!"))
+  }
+}
+
+.get_x_grid_title <- function(hypers) {
+  if (length(hypers) > 1) {
+    return("model")
+  } else {
+    return(names(hypers))
+  }
+}
+
+.get_x_grid_labels <- function(hypers, grid) {
+  if (length(hypers) > 1) {
+    return(1:nrow(grid))
+  } else {
+    return(hypers[[1]])
+  }
 }
--- tests/testthat/test_gridSearch.R
+++ tests/testthat/test_gridSearch.R
@@ -1,25 +1,44 @@
 context("Grid Search")
 
 val <- SDMtune:::t
+bg <- SDMtune:::bg
 model <- SDMtune:::bm_maxent
 model_cv <- SDMtune:::bm_maxent_cv
-h <- list("fc" = c("l", "lq", "lqp"), "reg" = seq(.2, 2., .2),
-          "a" = 10000)
 
 test_that("Exceptions are thrown", {
+  h <- list("fc" = c("l", "lq", "lqp"), "reg" = seq(.2, 2., .2), "a" = 10000)
   # Throws exception if metric is aicc and env is not provided
-  expect_that(.checkGridSearchArgs(model, h, metric = "aicc"),
-              throws_error("You must provide the 'env' argument if you want to use the AICc metric!"))
+  expect_error(.checkGridSearchArgs(model, h, metric = "aicc"),
+               "You must provide the 'env' argument if you want to use the AICc metric!")
   # Throws exception if metric is aicc and model is SDMmodelCV
-  expect_that(.checkGridSearchArgs(model_cv, h, metric = "aicc"),
-              throws_error("Metric 'aicc' not allowed with SDMmodelCV objects!"))
+  expect_error(.checkGridSearchArgs(model_cv, h, metric = "aicc"),
+               "Metric 'aicc' not allowed with SDMmodelCV objects!")
   # Throws exception if model is SDMmodel metric is not aicc and test is not provided
-  expect_that(.checkGridSearchArgs(model, h, metric = "auc"),
-              throws_error("You need to provide a test dataset!"))
+  expect_error(.checkGridSearchArgs(model, h, metric = "auc"),
+               "You need to provide a test dataset!")
   # Throws exception if hypers includes 'a' and bg4test is not provided
-  expect_that(.checkGridSearchArgs(model, h, "auc", t),
-              throws_error("bg4test must be provided to tune background locations!"))
+  expect_error(.checkGridSearchArgs(model, h, "auc", t),
+               "bg4test must be provided to tune background locations!")
   # Throws exception if max hypers 'a' > than nrow bg4test
-  expect_that(.checkGridSearchArgs(model, h, "auc", t, bg4test = bg),
-              throws_error("Maximum number of 'a' hyperparameter cannot be more than 9000!"))
+  expect_error(.checkGridSearchArgs(model, h, "auc", t, bg4test = bg),
+               "Maximum number of 'a' hyperparameter cannot be more than 9000!")
+  # doesn't throw exception if 'a' is not in hypers and bg4test is not provided
+  h$a <- NULL
+  expect_error(.checkGridSearchArgs(model, h, "auc", t), NA)
+})
+
+test_that("The correct x title is generated", {
+  h <- list("fc" = c("l", "lq", "lqp"), "reg" = seq(.2, 2., .2), "a" = 10000)
+  expect_equal(.get_x_grid_title(h), "model")
+  h <- list("fc" = c("l", "lq", "lqp"))
+  expect_equal(.get_x_grid_title(h), "fc")
+})
+
+test_that("The correct x labels are generated", {
+  h <- list("fc" = c("l", "lq", "lqp"), "reg" = seq(.2, 2., .2), "a" = 10000)
+  grid <- .get_hypers_grid(bm_maxent, h)
+  expect_equal(.get_x_grid_labels(h, grid), 1:nrow(grid))
+  h <- list("fc" = c("l", "lq", "lqp"))
+  grid <- .get_hypers_grid(bm_maxent, h)
+  expect_equal(.get_x_grid_labels(h, grid), h[[1]])
 })
