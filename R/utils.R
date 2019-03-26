2880a6287ba01a9d8653b3849f0cbdedc26218bb
Sergio Vignali <vignalisergio30@gmail.com>
2019-03-26 16:41:57 +0100

    move chart utils in chart_utils file, fix bug in real-time chart for non RStudio

--- R/utils.R
+++ R/utils.R
@@ -151,62 +151,6 @@
   return(output)
 }
 
-.start_server <- function(folder, name) {
-  port <- tools::startDynamicHelp(NA)
-  url <- paste0("http://127.0.0.1:", port, "/session/", basename(folder), name)
-  utils::browseURL(url)
-}
-
-.create_chart <- function(folder, script, settings, data, height = 300) {
-
-  dir.create(folder)
-
-  # Copy libraries and style files in lib directory
-  dir.create(file.path(folder, "lib"))
-  files <- list.files(system.file("lib", package = "SDMtune"),
-                      full.names = TRUE)
-  file.copy(files, file.path(folder, "lib"))
-
-  # Copy chart template
-  file.copy(file.path(system.file("templates", package = "SDMtune"),
-                      "chart_template.html"),
-            folder)
-
-  # render script
-  .render_script(folder, script, settings, data)
-
-  path <- file.path(folder, "chart_template.html")
-  viewer <- getOption("viewer")
-  if (!is.null(viewer)) {
-    viewer(path, height = height)  # Show chart in viewer pane
-  } else {
-    .start_server(folder, "/chart.html")  # Show chart in browser
-  }
-  Sys.sleep(.1)
-}
-
-#' @importFrom jsonlite toJSON
-#' @importFrom whisker whisker.render
-.render_script <- function(folder, script, settings, data) {
-
-  template <- paste(readLines(file.path(system.file("scripts",
-                                                    package = "SDMtune"),
-                                        script),
-                              encoding = "UTF-8"),
-                    collapse = "\n")
-
-  data <- list(settings = jsonlite::toJSON(settings),
-               data = jsonlite::toJSON(data))
-
-  rendered_script <- whisker::whisker.render(template, data = data)
-  writeLines(rendered_script, file.path(folder, "lib", "chart_script.js"))
-}
-
-#' @importFrom jsonlite write_json
-.update_chart <- function(folder, data) {
-  jsonlite::write_json(data, file.path(folder, "data.json"))
-}
-
 .get_train_args <- function(model) {
 
   args <- list(p = model@p, a = model@a)
