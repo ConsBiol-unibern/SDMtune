5e89fe802650ce91eeab9129f3298e6a25957106
Sergio Vignali <vignalisergio30@gmail.com>
2019-04-03 19:55:29 +0200

    Add skip_if covr is running

--- tests/testthat/test_maxent_prediction.R
+++ tests/testthat/test_maxent_prediction.R
@@ -1,8 +1,11 @@
-context("Maxent Predicion")
+context("Maxent Prediction")
 
 model <- SDMtune:::bm_maxent
 maxent_model <- SDMmodel2MaxEnt(model)
 data <- SDMtune:::bg
+
+skip_if(Sys.getenv("R_COVR") == "true",
+        message = "Skip Maxent Prediction test with covr!")
 
 test_that("The function predicts correctly", {
   expect_equal(predict(model, data, type = "cloglog"),
