24071833a4c178b8d1fbf159393093c6527f4edf
sgvignali <vignalisergio30@gmail.com>
2019-05-15 14:28:30 +0200

    Add tests for SDMmodel2MaxEnt, remove unnecessary double quotes from article in webpage

---
+++ tests/testthat/test_sdmmodel2maxent.R
@@ -0,0 +1,12 @@
+context("SDMmodel to Maxent")
+
+test_that("The function creates the cirrect output", {
+  expect_s4_class(SDMmodel2MaxEnt(SDMtune:::bm_maxent), "MaxEnt")
+  expect_equal(SDMmodel2MaxEnt(SDMtune:::bm_maxent)@presence,
+               SDMtune:::bm_maxent@p@data)
+  expect_equal(SDMmodel2MaxEnt(SDMtune:::bm_maxent)@absence,
+               SDMtune:::bm_maxent@a@data)
+  expect_equal(SDMmodel2MaxEnt(SDMtune:::bm_maxent)@lambdas,
+               SDMtune:::bm_maxent@model@lambdas)
+  expect_true(SDMmodel2MaxEnt(SDMtune:::bm_maxent)@hasabsence)
+})
