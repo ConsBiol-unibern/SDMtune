78539876e1a912cc4d5c5a417d33d09818a8e8f4
Sergio Vignali <22011122+sgvignali@users.noreply.github.com>
2019-05-22 15:05:10 +0200

    Remove wrong test

--- tests/testthat/test_plot_response.R
+++ tests/testthat/test_plot_response.R
@@ -6,7 +6,3 @@ test_that("Error are raised", {
   expect_error(plotResponse(model, "biowhat", "cloglog"),
                "biowhat is not used to train the model!")
 })
-
-test_that("An invisible object of class ggplot is returned", {
-          expect_equal(class(plotResponse(m, "bio1", "cloglog"))[2], "ggplot")
-})
