f81a73a5e968672fc05d34822862979ee9d4f86e
sgvignali <vignalisergio30@gmail.com>
2019-06-04 13:59:38 +0200

    Add test for thinData function

---
+++ tests/testthat/test_thin_data.R
@@ -0,0 +1,23 @@
+context("Thin data")
+
+files <- list.files(path = paste(system.file(package = "dismo"),
+                                 "/ex", sep = ""),
+                    pattern = "grd", full.names = TRUE)
+predictors <- raster::stack(files)
+set.seed(25)
+x <- dismo::randomPoints(predictors, 9000)
+c <- thinData(x, predictors)
+
+test_that("The function remove coords where there are NA", {
+  expect_equal(nrow(c), 8991)
+})
+
+test_that("The function remove duplicated data", {
+  c1 <- thinData(rbind(c, c), predictors)
+  expect_equal(nrow(c), 8991)
+})
+
+test_that("The correct output is returned", {
+  expect_equal(class(c), "data.frame")
+  expect_named(c, colnames(x))
+})
