0d53bcf4b589b09661983875adeb9a21da51a787
sgvignali <vignalisergio30@gmail.com>
2019-06-05 16:02:57 +0200

    Add tests for plotResponse and plotVarImp functions

--- tests/testthat/test_plot_response.R
+++ tests/testthat/test_plot_response.R
@@ -1,8 +1,33 @@
 context("Plot Response")
 
-model <- SDMtune:::bm_maxnet
+skip_on_cran()
+
+m <- SDMtune:::bm_maxnet
+m_cv <- SDMtune:::bm_maxnet_cv
 
 test_that("Error are raised", {
-  expect_error(plotResponse(model, "biowhat", "cloglog"),
+  expect_error(plotResponse(m, "biowhat", "cloglog"),
                "biowhat is not used to train the model!")
+})
+
+test_that("Labels and output are correct for SDMmodel objects", {
+  p <- plotResponse(m, "bio1", "cloglog", rug = TRUE, marginal = TRUE)
+  expect_length(p$layers, 3)  # line and two rugs
+  expect_equal(p$labels$x, "bio1")
+  expect_equal(p$labels$y, "cloglog output")
+  expect_true(min(p$data$y) >= 0)
+  expect_true(max(p$data$y) <= 1)
+  p <- plotResponse(m, "bio1", "logistic", marginal = FALSE)
+  expect_length(p$layers, 1)  # only line
+  expect_equal(p$labels$x, "bio1")
+  expect_equal(p$labels$y, "logistic output")
+  expect_true(min(p$data$y) >= 0)
+  expect_true(max(p$data$y) <= 1)
+})
+
+test_that("Labels and output are correct for SDMmodelCV objects", {
+  p <- plotResponse(m_cv, "bio1", "exponential", rug = TRUE, marginal = TRUE)
+  expect_length(p$layers, 4)  # line, ribbon and two rugs
+  expect_equal(p$labels$x, "bio1")
+  expect_equal(p$labels$y, "exponential output")
 })
