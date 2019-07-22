f58f6b6e4466871386cfa1f67c11c47193eb2ef7
Sergio Vignali <22011122+sgvignali@users.noreply.github.com>
2019-07-22 12:32:02 +0200

    Add pa slot to SWD object and adapt prepareSWD function

--- R/SWD_class.R
+++ R/SWD_class.R
@@ -7,7 +7,12 @@
 #' @slot species character. Name of the species.
 #' @slot coords data.frame. Coordinates of the locations.
 #' @slot data data.frame. Value of the environmental variables at location
-#' places.
+#' sites.
+#' @slot pa numeric. Vector with \code{1} for presence and \code{0} for
+#' absence/background locations.
+#'
+#' @details \code{data} and \code{coords} must have the same number of rows;
+#' \code{pa} must have length equal to \code{row(data)}.
 #'
 #' @export
 #'
@@ -16,16 +21,22 @@ SWD <- setClass("SWD",
                 representation(
                   species = "character",
                   coords = "data.frame",
-                  data = "data.frame"),
+                  data = "data.frame",
+                  pa = "numeric"),
                 prototype(
                   species = NA_character_,
                   coords = data.frame(),
-                  data = data.frame()
+                  data = data.frame(),
+                  pa = numeric()
                 ),
                 validity = function(object)	{
                   if (nrow(object@coords) != nrow(object@data))
                     return("coords and data have a different number of rows!")
-
+                  # TODO remove statement if (1 %in% object@pa)
+                  if (1 %in% object@pa) {
+                    if (nrow(object@coords) != length(object@pa))
+                      return("coords and pa have different length!")
+                  }
                   return(TRUE)
                 }
 )
@@ -45,7 +56,13 @@ setMethod(
     cat("Object of class", class(object), "\n\n")
 
     cat("Species:", object@species, "\n")
-    cat("Locations:", nrow(object@data), "\n\n")
+    # TODO remove if statement
+    if (1 %in% object@pa) {
+      cat("Locations:", nrow(object@data), "\n\n")
+    } else {
+      cat("Presence locations:",  nrow(object@data[object@pa == 1, ]), "\n")
+      cat("Absence locations:",  nrow(object@data[object@pa == 0, ]), "\n\n")
+    }
 
     cat("Variables:\n")
     cat("---------\n")
--- R/prepareSWD.R
+++ R/prepareSWD.R
@@ -1,17 +1,18 @@
 #' Prepare an SWD object
 #'
 #' Given the coordinates, the species' name and the environmental variables,
-#' the function creates a \linkS4class{SWD} object (sample with data).
+#' the function creates an \linkS4class{SWD} object (sample with data).
 #'
 #' @param species character. The name of the species.
-#' @param coords data.frame. The coordinates of the presence or
-#' absence/background locations.
+#' @param coords Deprecated.
+#' @param p data.frame. The coordinates of the presence locations.
+#' @param a data.frame. The coordinates of the absence/background locations.
 #' @param env \code{\link[raster]{stack}} containing the environmental variables
 #' used to extract the values at coordinate locations.
 #' @param categorical vector indicating which of the environmental variable are
 #' categorical, default is \code{NULL}.
 #'
-#' @return A \linkS4class{SWD} object
+#' @return An \linkS4class{SWD} object.
 #' @export
 #' @importFrom stats complete.cases
 #'
@@ -26,41 +27,90 @@
 #' # Prepare presence locations
 #' p_coords <- condor[, 1:2]
 #'
-#' presence <- prepareSWD(species = "Vultur gryphus", coords = p_coords,
-#'                        env = predictors, categorical = "biome")
+#' # Prepare background locations
+#' set.seed(25)
+#' bg_coords <- dismo::randomPoints(predictors, 2000)
+#'
+#' train <- prepareSWD(species = "Vultur gryphus", p = p_coords, a = bg_coords,
+#'                     env = predictors, categorical = "biome")
 #' presence
-prepareSWD <- function(species, coords, env, categorical = NULL) {
+prepareSWD <- function(species, p = NULL, a = NULL, coords = NULL, env,
+                       categorical = NULL) {
 
-  coords <- as.data.frame(coords)
+  # TODO Remove it in the next release.
+  if (!is.null(coords)) {
+    warning("Argument \"coords\" is deprecated and will be removed in the next",
+            " release. Please use \"p\" and \"a\" instead.", call. = FALSE)
+    coords <- as.data.frame(coords)
+    message("Extracting predictor information for given locations...")
+    data <- as.data.frame(raster::extract(env, coords))
+    # Remove any occurrence point with NA for at least one variable
+    index <- complete.cases(data)
+    discarded <- nrow(data) - sum(index)
+    if (discarded > 0) {
+      data <- data[index, ]
+      coords <- coords[index, ]
+      message("Warning: ", discarded,
+              ifelse(discarded == 1, " location is", " locations are"),
+              " NA for some environmental variables, ",
+              ifelse(discarded == 1, "it ", "they "),
+              "will be discard!")
+    }
+    colnames(coords) <- c("X", "Y")
+    # Set categorical variables as factors
+    if (!is.null(categorical)) {
+      for (i in categorical) {
+        data[, i] <- as.factor(data[, i])
+      }
+    }
+    # Reset row names
+    rownames(coords) <- NULL
+    rownames(data) <- NULL
 
-  message("Extracting predictor information for given locations...")
-  data <- as.data.frame(raster::extract(env, coords))
+    swd <- SWD(species = species, coords = coords, data = data,
+               pa = NA_real_)
+  } else {
+    df_coords <- data.frame(X = numeric(), Y = numeric())
+    df_data <- p[0, ]
+    dfs <- list(p, a)
+    text <- c("presence", "absence/background")
+    for (i in 1:2) {
+      coords <- as.data.frame(dfs[[i]])
+      colnames(coords) <- c("X", "Y")
+      message("Extracting predictor information for ", text[i], " locations...")
+      data <- as.data.frame(raster::extract(env, coords))
+      # Remove any occurrence point with NA for at least one variable
+      index <- complete.cases(data)
+      discarded <- nrow(data) - sum(index)
+      if (discarded > 0) {
+        data <- data[index, ]
+        coords <- coords[index, ]
+        message("Warning: ", discarded, " ", text[i],
+                ifelse(discarded == 1, " location is", " locations are"),
+                " NA for some environmental variables, ",
+                ifelse(discarded == 1, "it ", "they "),
+                "will be discarded!")
+      }
+      df_coords <- rbind(df_coords, coords)
+      df_data <- rbind(df_data, data)
+      if (i == 1) {
+        pa <- rep(1, nrow(coords))
+      } else {
+        pa <- c(pa, rep(0, nrow(coords)))
+      }
+    }
+    # Set categorical variables as factors
+    if (!is.null(categorical)) {
+      for (i in categorical) {
+        df_data[, i] <- as.factor(df_data[, i])
+      }
+    }
+    # Reset row names
+    rownames(df_coords) <- NULL
+    rownames(df_data) <- NULL
 
-  # Remove any occurrence point with NA for at least one variable
-  index <- complete.cases(data)
-  discarded_locations <- nrow(data) - sum(index)
-  if (discarded_locations > 0) {
-    data <- data[index, ]
-    coords <- coords[index, ]
-    message(paste("Warning:", discarded_locations,
-                  ifelse(discarded_locations == 1, "location is",
-                         "locations are"),
-                  "NA for some environmental variables,",
-                  ifelse(discarded_locations == 1, "it", "they"),
-                  "will be discard!"))
+    swd <- SWD(species = species, coords = df_coords, data = df_data, pa = pa)
   }
 
-  colnames(coords) <- c("X", "Y")
-  # Set categorical variables as factors
-  if (!is.null(categorical)) {
-    for (i in categorical) {
-      data[, i] <- as.factor(data[, i])
-    }
-
-  # Reset row names
-  rownames(coords) <- NULL
-  rownames(data) <- NULL
-}
-  swd <- SWD(species = species, coords = coords, data = data)
   return(swd)
 }
