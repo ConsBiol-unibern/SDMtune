0fc95b77fd1a9756744479276033fa9e3e8901d6
Sergio Vignali <22011122+sgvignali@users.noreply.github.com>
2019-07-24 08:40:36 +0200

    Update swd2csv function

--- R/swd2csv.R
+++ R/swd2csv.R
@@ -1,9 +1,20 @@
 #' SWD to csv
 #'
-#' Save a \linkS4class{SWD} object as csv file.
+#' Save an \code{\linkS4class{SWD}} object as csv file.
 #'
-#' @param swd \linkS4class{SWD} object.
-#' @param file_name character. The name of the file where to save the object.
+#' @param swd \code{\linkS4class{SWD}} object.
+#' @param file_name character. The name of the file in which to save the object,
+#' see details.
+#'
+#' @details
+#' * The \code{file_name} argument should include the extension (i.e.
+#' my_file.csv).
+#' * If \code{file_name} is a single name the function saves the presence
+#' absence/background locations in a single file, adding the column **pa** with
+#' 1s for presence and 0s for absence/background locations. If \code{file_name}
+#' is a vector with two names, it saves the object in two files: the first name
+#' is used for the presence locations and the second for the absence/background
+#' locations.
 #'
 #' @export
 #' @importFrom utils write.csv
@@ -28,7 +39,15 @@
 #' swd2csv(train, "train_data.csv")
 #' }
 swd2csv <- function(swd, file_name) {
-  df <- cbind(swd@species, swd@coords, swd@data)
-  colnames(df)[1] <- "Species"
-  write.csv(df, file_name, row.names = FALSE)
+
+  if (length(file_name) == 2) {
+    df <- cbind(swd@species, swd@coords, swd@data)
+    colnames(df)[1] <- "Species"
+    write.csv(df[swd@pa == 1, ], file_name[1], row.names = FALSE)
+    write.csv(df[swd@pa == 0, ], file_name[2], row.names = FALSE)
+  } else {
+    df <- cbind(swd@species, swd@pa, swd@coords, swd@data)
+    colnames(df)[1:2] <- c("Species", "pa")
+    write.csv(df, file_name[1], row.names = FALSE)
+  }
 }
--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -11,20 +11,21 @@ p <- condor[, 1:2]
 set.seed(25)
 bg <- dismo::randomPoints(predictors[[9]], 5000)
 
-# Create train dataset
-train <- prepareSWD(species = "Vultur gryphus", p = p, a = bg, env = predictors,
-                    categorical = "biome")
+# Create train dataset, don't use the variable name train because in testthat
+# is interpreted as the train function when using SDMtune:::train
+t <- prepareSWD(species = "Vultur gryphus", p = p, a = bg, env = predictors,
+                categorical = "biome")
 
-folds <- randomFolds(data = train, k = 4, only_presence = TRUE)
+folds <- randomFolds(data = t, k = 4, only_presence = TRUE)
 
 # Train base models with default settings
 # Maxent
-bm_maxent <- train("Maxent", data = train)
-bm_maxent_cv <- train("Maxent", data = train, folds = folds)
+bm_maxent <- train("Maxent", data = t)
+bm_maxent_cv <- train("Maxent", data = t, folds = folds)
 
 # Maxnet
-bm_maxnet <- train("Maxnet", data = train)
-bm_maxnet_cv <- train("Maxnet", data = train, folds = folds)
+bm_maxnet <- train("Maxnet", data = t)
+bm_maxnet_cv <- train("Maxnet", data = t, folds = folds)
 
 # Feature Class mapping
 fc_map = list(
@@ -35,5 +36,5 @@ fc_map = list(
 )
 
 # save objects in sysdata
-usethis::use_data(train, bm_maxent, bm_maxent_cv, bm_maxnet, bm_maxnet_cv,
-                  fc_map, internal = TRUE, overwrite = TRUE)
+usethis::use_data(t, bm_maxent, bm_maxent_cv, bm_maxnet, bm_maxnet_cv, fc_map,
+                  internal = TRUE, overwrite = TRUE)
