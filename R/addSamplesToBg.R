00eb321820cb476d618f529ed18ade02f2bd639a
sgvignali <vignalisergio30@gmail.com>
2019-12-10 14:56:37 +0100

    Update doc

--- R/addSamplesToBg.R
+++ R/addSamplesToBg.R
@@ -27,19 +27,21 @@
 #' data <- prepareSWD(species = "Virtual species", p = p_coords, a = bg_coords,
 #'                    env = predictors, categorical = "biome")
 #'
-#' # Add to the background locations presence locations with values not included
-#' # in the background
+#' # Add presence locations with values not included in the background to the
+#' # background locations
 #' new_data <- addSamplesToBg(data)
 #' new_data
 #'
-#' # Add to the background locations all the presence locations even if they
-#' have values already included in the background
+#' # Add all the presence locations to the background locations, even if they
+#' # have values already included in the background
 #' new_data <- addSamplesToBg(data, all = TRUE)
 #' new_data
 addSamplesToBg <- function(x, all = FALSE) {
 
   if (class(x) != "SWD")
     stop("The function accepts only SWD objects.")
+
+  # Append presence locations after background locations
   o <- x
   o@data <- rbind(x@data, x@data[x@pa == 1, ])
   rownames(o@data) <- NULL
@@ -48,10 +50,13 @@ addSamplesToBg <- function(x, all = FALSE) {
   o@pa <- c(x@pa, rep(0, sum(x@pa)))
 
   if (!all) {
+    # Get index of duplicated background locations
     index <- duplicated(o@data[o@pa == 0, ])
     index <- which(index == TRUE)
+    # Get index only for the appended presence locations
     index <- index[index > sum(x@pa == 0)]
     if (length(index) > 0) {
+      # Remove duplicates
       o@data <- o@data[-index, ]
       rownames(o@data) <- NULL
       o@coords <- o@coords[-index, ]
