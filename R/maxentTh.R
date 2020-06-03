f038a099a5e89db9fb83c8e227e9a9b5c740223b
sgvignali <vignalisergio30@gmail.com>
2020-06-03 12:32:10 +0200

    Update example

--- R/maxentTh.R
+++ R/maxentTh.R
@@ -12,7 +12,6 @@
 #' @seealso \code{\link{maxentVarImp}}
 #'
 #' @examples
-#' \donttest{
 #' # Acquire environmental variables
 #' files <- list.files(path = file.path(system.file(package = "dismo"), "ex"),
 #'                     pattern = "grd", full.names = TRUE)
@@ -27,6 +26,9 @@
 #'                    env = predictors, categorical = "biome")
 #'
 #' # Train a Maxent model
+#' # The next line checks if Maxent is correctly configured but you don't need
+#' # to run it in your script
+#' if (checkMaxentInstallation(verbose = FALSE)) {
 #' model <- train(method = "Maxent", data = data, fc = "l")
 #' maxentTh(model)
 #' }
