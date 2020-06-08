da7fbb1bace36e48dded2866a050897fa8827d9c
sgvignali <vignalisergio30@gmail.com>
2020-06-08 20:01:46 +0200

    Clean doc

--- R/ANN-class.R
+++ R/ANN-class.R
@@ -8,9 +8,9 @@ setOldClass("nnet")
 #' @slot decay numeric. Weight decay.
 #' @slot rang numeric. Initial random weights.
 #' @slot maxit integer. Maximum number of iterations.
-#' @slot model \code{\link{nnet}}. The randomForest model object.
+#' @slot model \link{nnet}. The randomForest model object.
 #'
-#' @details See \code{\link{nnet}} for the meaning of the slots.
+#' @details See \link{nnet} for the meaning of the slots.
 #'
 #' @aliases NULL ANN-class
 #' @export
