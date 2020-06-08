b6a5ea90e557fe724e6cbcbd4a23eab9e7b20f5a
sgvignali <vignalisergio30@gmail.com>
2020-06-08 21:19:49 +0200

    Clean doc

--- R/optimizeModel.R
+++ R/optimizeModel.R
@@ -3,19 +3,18 @@
 #' The function uses a Genetic Algorithm implementation to optimize the model
 #' hyperparameter configuration according to the chosen metric.
 #'
-#' @param model \code{\linkS4class{SDMmodel}} or \code{\linkS4class{SDMmodelCV}}
-#' object.
+#' @param model \linkS4class{SDMmodel} or \linkS4class{SDMmodelCV} object.
 #' @param hypers named list containing the values of the hyperparameters that
 #' should be tuned, see details.
 #' @param metric character. The metric used to evaluate the models, possible
 #' values are: "auc", "tss" and "aicc".
-#' @param test \code{\linkS4class{SWD}} object. Test dataset used to evaluate
-#' the model, not used with aicc and \code{\linkS4class{SDMmodelCV}} objects,
-#' default is \code{NULL}.
+#' @param test \linkS4class{SWD} object. Testing dataset used to evaluate
+#' the model, not used with aicc and \linkS4class{SDMmodelCV} objects, default
+#' is `NULL`.
 #' @param pop numeric. Size of the population, default is 5.
 #' @param gen numeric. Number of generations, default is 20.
-#' @param env \code{\link[raster]{stack}} containing the environmental
-#' variables, used only with "aicc", default is \code{NULL}.
+#' @param env \link[raster]{stack} containing the environmental variables, used
+#' only with "aicc", default is `NULL`.
 #' @param parallel deprecated.
 #' @param keep_best numeric. Percentage of the best models in the population to
 #' be retained during each iteration, expressed as decimal number. Default
@@ -25,23 +24,22 @@
 #' @param mutation_chance numeric. Probability of mutation of the child models,
 #' expressed as decimal number. Default is 0.4.
 #' @param seed numeric. The value used to set the seed to have consistent
-#' results, default is \code{NULL}.
+#' results, default is `NULL`.
 #'
 #' @details To know which hyperparameters can be tuned you can use the output
-#' of the function \code{\link{getTunableArgs}}. Hyperparameters not included
-#' in the \code{hypers} argument take the value that they have in the passed
-#' model.
+#' of the function \link{getTunableArgs}. Hyperparameters not included in the
+#' `hypers` argument take the value that they have in the passed model.
 #' * Part of the code is inspired by
 #' \href{https://blog.coast.ai/lets-evolve-a-neural-network-with-a-geneticalgorithm-code-included-8809bece164}{this post}.
 #'
-#' @return \code{\linkS4class{SDMtune}} object.
+#' @return \linkS4class{SDMtune} object.
 #' @export
 #' @importFrom progress progress_bar
 #' @importFrom stats runif
 #'
 #' @author Sergio Vignali
 #'
-#' @seealso \code{\link{gridSearch}} and \code{\link{randomSearch}}.
+#' @seealso \link{gridSearch} and \link{randomSearch}.
 #'
 #' @examples
 #' \donttest{
