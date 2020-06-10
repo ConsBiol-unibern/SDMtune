1119f5304fab3ba2899fa173a88424ee09cf1ce9
sgvignali <vignalisergio30@gmail.com>
2020-06-10 18:11:49 +0200

    clean importFrom

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,11 @@
-#' @importFrom utils packageVersion
 text <- paste0("
    _____  ____   __  ___ __
   / ___/ / __ \\ /  |/  // /_ __  __ ____   ___
   \\__ \\ / / / // /|_/ // __// / / // __ \\ / _ \\
  ___/ // /_/ // /  / // /_ / /_/ // / / //  __/
 /____//_____//_/  /_/ \\__/ \\__,_//_/ /_/ \\___/  version ",
-               packageVersion("SDMtune"),
-               "\n\nTo cite this package in publications type: citation(\"SDMtune\").")
+  utils::packageVersion("SDMtune"),
+  "\n\nTo cite this package in publications type: citation(\"SDMtune\").")
 
 
 .onAttach <- function(libname, pkgname) {
