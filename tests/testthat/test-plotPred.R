e3af8bfe49922b2bf4286740dea9a7e587f24984
sgvignali <vignalisergio30@gmail.com>
2021-03-14 11:47:33 +0100

    Remove test for maxpixels not included anymore in plot_env with rsterVis 0.50

--- tests/testthat/test-plotPred.R
+++ tests/testthat/test-plotPred.R
@@ -7,10 +7,8 @@ test_that("The function raises an error if argument is not a raster", {
 
 test_that("The values are correct", {
   p <- plotPred(map)
-  expect_equal(p$plot_env$maxpixels, 50000)
   expect_true(min(p$data$value) >= 0)
   expect_true(max(p$data$value) <= 1)
   # If hr is TRUE it should use the number of pixel in the raster
   p <- plotPred(map, hr = TRUE)
-  expect_equal(p$plot_env$maxpixels, 400)
 })
