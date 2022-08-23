d44ffa34ae70acaaafb448d7d67eb0f02cdb3081
sgvignali <22011122+sgvignali@users.noreply.github.com>
2022-08-23 10:23:08 +0200

    Use new version of roxygen to solve CRAN notes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // scaleClamp
 NumericMatrix scaleClamp(NumericMatrix x, NumericVector min, NumericVector max, LogicalVector do_clamp, LogicalVector scale);
 RcppExport SEXP _SDMtune_scaleClamp(SEXP xSEXP, SEXP minSEXP, SEXP maxSEXP, SEXP do_clampSEXP, SEXP scaleSEXP) {
