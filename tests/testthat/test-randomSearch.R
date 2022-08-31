e89b62c75b878f227036a76b2147a4c21bb8963b
sgvignali <22011122+sgvignali@users.noreply.github.com>
2022-08-31 12:38:25 +0200

    Add tests to check interactive chart

--- tests/testthat/test-randomSearch.R
+++ tests/testthat/test-randomSearch.R
@@ -4,10 +4,16 @@ files <- list.files(path = file.path(system.file(package = "dismo"), "ex"),
                     pattern = "grd", full.names = TRUE)
 predictors <- raster::stack(files)
 h <- list(fc = c("l", "q", "p"), reg = 1:3)
-o <- randomSearch(SDMtune:::bm_maxnet, h, "aicc", pop = 3, env = predictors,
-                  seed = 25)
+
+test_that("The interactive chart is not created", {
+  randomSearch(SDMtune:::bm_maxnet, h, "aicc", pop = 3, env = predictors,
+               interactive = FALSE, seed = 25)
+  expect_false(any(grepl("SDMtune-randomSearch", list.dirs(tempdir()))))
+})
 
 test_that("randomSearch produces the expected output", {
+  o <- randomSearch(SDMtune:::bm_maxnet, h, "aicc", pop = 3, env = predictors,
+                    seed = 25)
   expect_s4_class(o, "SDMtune")
   expect_s4_class(o@models[[1]], "SDMmodel")
   expect_s3_class(o@results, "data.frame")
@@ -15,4 +21,5 @@ test_that("randomSearch produces the expected output", {
   expect_length(o@models, 3)
   expect_equal(o@results$fc, c("q", "l", "l"))
   expect_equal(o@results$reg, c(2, 2, 3))
+  expect_true(any(grepl("SDMtune-randomSearch", list.dirs(tempdir()))))
 })
