f76b013a428fb7a3fd4a8419ebcd58e2536732bc
sgvignali <22011122+sgvignali@users.noreply.github.com>
2022-09-07 09:52:47 +0200

    Add tests for model report

---
+++ tests/testthat/_snaps/modelReport.md
@@ -0,0 +1,73 @@
+# Settings are correct
+
+              
+      * Model type: Maxent
+      * Train locations: 5400
+          * presence: 400
+          * absence/background: 5000
+      * Test locations: 5400
+          * presence: 400
+          * absence/background: 5000
+      * Continuous variables: bio1, bio12, bio16, bio17, bio5, bio6, bio7, bio8
+      * Categorical variables: biome
+      * Output type: cloglog
+      * Feature Class combination: lqph
+      * Regularization multiplier: 1
+      * Do clamping for predictions: TRUE,
+      * Extra arguments: removeduplicates=false, addsamplestobackground=false
+
+---
+
+        
+      * Model type: Maxnet
+      * Train locations: 5400
+          * presence: 400
+          * absence/background: 5000
+      * Continuous variables: bio1, bio12, bio16, bio17, bio5, bio6, bio7, bio8
+      * Categorical variables: biome
+      * Output type: cloglog
+      * Feature Class combination: lqph
+      * Regularization multiplier: 1
+
+---
+
+        
+      * Model type: ANN
+      * Train locations: 5400
+          * presence: 400
+          * absence/background: 5000
+      * Continuous variables: bio1, bio12, bio16, bio17
+      * Categorical variables: 
+      * Size: 10
+      * Decay: 0
+      * Rang: 0.7
+      * Maxit: 100
+
+---
+
+        
+      * Model type: BRT
+      * Train locations: 5400
+          * presence: 400
+          * absence/background: 5000
+      * Continuous variables: bio1, bio12, bio16, bio17
+      * Categorical variables: ,
+      * Distribution: bernoulli
+      * Number of trees: 200
+      * Interaction depth: 1
+      * Shrinkage: 0.2
+      * Bag fraction: 0.5
+
+---
+
+        
+      * Model type: RF
+      * Train locations: 5400
+          * presence: 400
+          * absence/background: 5000
+      * Continuous variables: bio1, bio12, bio16, bio17
+      * Categorical variables: ,
+      * Mtry: 2
+      * Number of trees: 200
+      * Node size: 1
+
--- tests/testthat/test-modelReport.R
+++ tests/testthat/test-modelReport.R
@@ -1,8 +1,22 @@
 skip_on_cran()
 
+files <- list.files(path = file.path(system.file(package = "dismo"), "ex"),
+                    pattern = "grd", full.names = TRUE)
+predictors <- raster::stack(files)
+folder <- "trash"
 test_that("All files are created", {
-  folder <- "trash"
-  create_local_model_report(folder = folder)
+
+  modelReport(SDMtune:::bm_maxnet,
+              type = "cloglog",
+              folder = folder,
+              test = SDMtune:::t,
+              permut = 1,
+              env = predictors,
+              jk = TRUE,
+              response_curves = TRUE,
+              verbose = FALSE)
+
+  withr::defer(unlink(file.path(getwd(), folder), recursive = TRUE))
 
   expect_true(file.exists(file.path(folder, "train.csv")))
   expect_true(file.exists(file.path(folder, "test.csv")))
@@ -10,4 +24,54 @@ test_that("All files are created", {
   expect_true(file.exists(file.path(folder, "map.tif")))
   expect_true(file.exists(file.path(folder, "virtual_species.html")))
   expect_true(file.exists(file.path(folder, "plots", "ROC_curve.png")))
+  expect_true(file.exists(file.path(folder, "plots", "map.png")))
+  expect_true(file.exists(file.path(folder, "plots", "train_jk.png")))
+  expect_true(file.exists(file.path(folder, "plots", "test_jk.png")))
+
+  for (var in names(predictors)) {
+    expect_true(file.exists(
+      file.path(folder, "plots", paste0(var, "_marginal.png"))
+    ))
+    expect_true(file.exists(
+      file.path(folder, "plots", paste0(var, "_univariate.png"))
+    ))
+  }
+})
+
+test_that("Settings are correct", {
+  data <- SDMtune:::t
+  data@data <- data@data[, 1:4]
+  m_ann <- train("ANN", data = data, size = 10)
+  m_brt <- train("BRT", data = data, n.trees = 200, shrinkage = 0.2)
+  m_rf <- train("RF", data = data, mtry = 2, ntree = 200)
+  params = list(model = SDMtune:::bm_maxent,
+                type = "cloglog",
+                test = SDMtune:::t,
+                folder = folder,
+                plot_folder = file.path(folder, "plots"),
+                env = predictors,
+                jk = FALSE,
+                response_curves = FALSE,
+                only_presence = FALSE,
+                clamp = TRUE,
+                permut = 1,
+                factors = NULL,
+                verbose = FALSE)
+
+  # Maxent with training and testing datasets and prediction
+  expect_snapshot_output(.write_report_model_settings(params))
+  # Maxnet without testing datasets and prediction
+  params$model <- SDMtune:::bm_maxnet
+  params$test <- NULL
+  params$env <- NULL
+  expect_snapshot_output(.write_report_model_settings(params))
+  # ANN
+  params$model <- m_ann
+  expect_snapshot_output(.write_report_model_settings(params))
+  # BRT
+  params$model <- m_brt
+  expect_snapshot_output(.write_report_model_settings(params))
+  # RF
+  params$model <- m_rf
+  expect_snapshot_output(.write_report_model_settings(params))
 })
