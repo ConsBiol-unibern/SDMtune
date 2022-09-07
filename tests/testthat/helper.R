fdba8f7aeeedc962a71dd474df696bce0bb9ed0e
sgvignali <22011122+sgvignali@users.noreply.github.com>
2022-09-07 09:52:29 +0200

    Delete unused fixture

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,4 +1,8 @@
-create_local_chart <- function(folder, script, update, env = parent.frame()) {
+create_local_chart <- function(folder,
+                               script,
+                               update,
+                               env = parent.frame()) {
+
   settings <- list(update = FALSE)
   data <- list()
   .create_chart(folder = folder, script = script, settings = settings,
@@ -6,21 +10,4 @@ create_local_chart <- function(folder, script, update, env = parent.frame()) {
   if (update)
     .update_data(folder, data = settings)
   withr::defer(unlink(folder, recursive = TRUE), envir = env)
-}
-
-create_local_model_report <- function(folder, env = parent.frame()) {
-
-  if (file.exists(file.path(getwd(), folder)))
-    unlink(file.path(getwd(), folder), recursive = TRUE)
-
-  m <- SDMtune:::bm_maxnet
-  files <- list.files(path = file.path(system.file(package = "dismo"), "ex"),
-                      pattern = "grd", full.names = TRUE)
-  env_vars <- raster::stack(files)
-
-  modelReport(m, type = "cloglog", folder = folder, test = SDMtune:::t,
-              permut = 1, env = env_vars, verbose = FALSE)
-
-  withr::defer(unlink(file.path(getwd(), folder), recursive = TRUE),
-               envir = env)
 }
