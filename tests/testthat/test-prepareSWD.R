e8783b79ea9c314bc80d6ac55d98dba57a61f601
sgvignali <22011122+sgvignali@users.noreply.github.com>
2022-09-08 09:30:15 +0200

    Suppress messages

--- tests/testthat/test-prepareSWD.R
+++ tests/testthat/test-prepareSWD.R
@@ -9,8 +9,8 @@ p <- rbind(c(10, 10), p)
 a <- virtualSp$background
 
 test_that("Output is correct", {
-  swd <- prepareSWD(species = "Gypaetus barbatus", p = p, a = a,
-                    env = env, categorical = "biome", verbose = FALSE)
+  swd <- suppressMessages(prepareSWD(species = "Gypaetus barbatus", p = p,
+                                     a = a, env = env, categorical = "biome"))
   expect_s4_class(swd, "SWD")
   expect_equal(swd@species, "Gypaetus barbatus")
   expect_named(swd@data, names(env))
