3313695c7f7d6f720aacb1485a1dec0de967ab4a
sgvignali <22011122+sgvignali@users.noreply.github.com>
2022-09-30 14:51:32 +0200

    Refactor examples using terra

--- R/corVar.R
+++ R/corVar.R
@@ -8,9 +8,9 @@
 #' @param method character. The method used to compute the correlation matrix.
 #' @param cor_th numeric. If provided it prints only the variables whose
 #' correlation coefficient is higher or lower than the given threshold.
-#' @param order logical, if `TRUE` the variable are ordered from the most to the
+#' @param order logical. If `TRUE` the variable are ordered from the most to the
 #' less highly correlated.
-#' @param remove_diagonal logical, if `TRUE` the values in the diagonal are,
+#' @param remove_diagonal logical. If `TRUE` the values in the diagonal are
 #' removed.
 #'
 #' @return A data.frame with the variables and their correlation.
@@ -21,22 +21,34 @@
 #' @examples
 #' # Acquire environmental variables
 #' files <- list.files(path = file.path(system.file(package = "dismo"), "ex"),
-#'                     pattern = "grd", full.names = TRUE)
-#' predictors <- raster::stack(files)
+#'                     pattern = "grd",
+#'                     full.names = TRUE)
+#'
+#' predictors <- terra::rast(files)
 #'
 #' # Prepare background locations
-#' bg_coords <- dismo::randomPoints(predictors, 10000)
+#' bg_coords <- terra::spatSample(predictors,
+#'                                size = 10000,
+#'                                method = "random",
+#'                                na.rm = TRUE,
+#'                                xy = TRUE,
+#'                                values = FALSE)
 #'
 #' # Create SWD object
-#' bg <- prepareSWD(species = "Virtual species", a = bg_coords,
-#'                  env = predictors, categorical = "biome")
+#' bg <- prepareSWD(species = "Virtual species",
+#'                  a = bg_coords,
+#'                  env = predictors,
+#'                  categorical = "biome")
 #'
 #' # Get the correlation among all the environmental variables
-#' corVar(bg, method = "spearman")
+#' corVar(bg,
+#'        method = "spearman")
 #'
 #' # Get the environmental variables that have a correlation greater or equal to
 #' # the given threshold
-#' corVar(bg, method = "pearson", cor_th = 0.8)
+#' corVar(bg,
+#'        method = "pearson",
+#'        cor_th = 0.8)
 corVar <- function(bg,
                    method = "spearman",
                    cor_th = NULL,
@@ -71,5 +83,5 @@ corVar <- function(bg,
     df <- df[order(-abs(df$value)), ]
   rownames(df) <- NULL
 
-  return(df)
+  df
 }
